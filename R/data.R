99a055671945e049982c0b496e544a05f0d589e4
stefanocoretta <s.coretta@ed.ac.uk>
2025-09-07 17:32:04 +0100

    update data

--- R/data.R
+++ R/data.R
@@ -11,6 +11,7 @@
 #'   \item{bii_logit_mean}{Mean of logit-transformed BII.}
 #'   \item{bii_logit_se}{Standard error of mean logit-transformed BII.}
 #'   \item{n_langs}{Number of languages.}
+#'   \item{log_n_langs}{Logged number of languages.}
 #' }
 #' @source
 #' - Helen Phillips; Adriana De Palma; Ricardo E Gonzalez; Sara Contu et al. (2021). The Biodiversity Intactness Index - country, region and global-level summaries for the year 1970 to 2050 under various scenarios \[Data set\]. Natural History Museum. <https://doi.org/10.5519/he1eqmg1>.
