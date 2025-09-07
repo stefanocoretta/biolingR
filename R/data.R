b12f6f23c0a8769f91ba8733c55dbbb7be71365b
stefanocoretta <s.coretta@ed.ac.uk>
2025-09-07 13:51:14 +0100

    init package

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Biodiversity intactness and number of languages
+#'
+#' A tibble with mean biodiversity intactness index (BII) and number of languages by country.
+#' The BII data is from Phillips et al. 2021.
+#'
+#' @format A tibble with 241 rows and 6 variables:
+#' \describe{
+#'   \item{ISO3}{Country ISO3 code.}
+#'   \item{country}{Name of Country.}
+#'   \item{bii_mean}{Mean BII.}
+#'   \item{bii_logit_mean}{Mean of logit-transformed BII.}
+#'   \item{bii_logit_se}{Standard error of mean logit-transformed BII.}
+#'   \item{n_langs}{Number of languages.}
+#' }
+#' @source
+#' - Helen Phillips; Adriana De Palma; Ricardo E Gonzalez; Sara Contu et al. (2021). The Biodiversity Intactness Index - country, region and global-level summaries for the year 1970 to 2050 under various scenarios \[Data set\]. Natural History Museum. <https://doi.org/10.5519/he1eqmg1>.
+#' - Glottolog 5.2 https://glottolog.org.
+"bii_nlangs"
---
+++ data-raw/raw/bii/README.md
@@ -0,0 +1,6 @@
+This is data from the project *The Biodiversity Intactness Index*.
+
+The data is available at <https://doi.org/10.5519/he1eqmg1> under a CC-NC license.
+
+> Helen Phillips; Adriana De Palma; Ricardo E Gonzalez; Sara Contu et al. (2021). The Biodiversity Intactness Index - country, region and global-level summaries for the year 1970 to 2050 under various scenarios [Data set]. Natural History Museum. https://doi.org/10.5519/he1eqmg1
+
