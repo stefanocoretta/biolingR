e12f2bbe7a224b91fe563cf8c1ca3a12b2cda209
stefanocoretta <s.coretta@ed.ac.uk>
2025-09-12 18:21:58 +0100

    update data

--- R/data.R
+++ R/data.R
@@ -7,16 +7,18 @@
 #'   \item{iso3c}{Country ISO3 code.}
 #'   \item{iso2c}{Country ISO2 code.}
 #'   \item{country}{Name of Country.}
-#'   \item{bii_mean}{Mean BII.}
-#'   \item{bii_logit_mean}{Mean of logit-transformed BII.}
-#'   \item{bii_logit_se}{Standard error of mean logit-transformed BII.}
+#'   \item{BII_mean}{Mean BII.}
+#'   \item{BII_logit_mean}{Mean of logit-transformed BII.}
+#'   \item{BII_logit_se}{Standard error of mean logit-transformed BII.}
 #'   \item{n_langs}{Number of languages.}
 #'   \item{log_n_langs}{Logged number of languages.}
+#'   \item{EI}{Endangerment index (proportion).}
+#'   \item{EI_logit}{Logit-transformed ndangerment index (proportion).}
 #'   \item{year}{Year of data.}
 #'   \item{AG.SRF.TOTL.K2}{Total country surface in km^2.}
 #'   \item{SP.POP.TOTL}{Total country population.}
-#'   \item{log_area}{Logged total country surface.}
-#'   \item{log_pop}{Logged total country population.}
+#'   \item{area_log}{Logged total country surface.}
+#'   \item{pop_log}{Logged total country population.}
 #' }
 #' @source
 #' - Helen Phillips; Adriana De Palma; Ricardo E Gonzalez; Sara Contu et al. (2021). The Biodiversity Intactness Index - country, region and global-level summaries for the year 1970 to 2050 under various scenarios \[Data set\]. Natural History Museum. <https://doi.org/10.5519/he1eqmg1>.
